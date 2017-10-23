66576e735cda90203c2484c1834e9c0ba8ee9ae1
闻端Carl <wddwyss@gmail.com>
2017-10-23 23:33:22 +0900

    move code to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,10 @@
-use std::io::prelude::*;
+extern crate minigrep;
+
 use std::env;
-use std::fs::File;
-use std::error::Error;
 use std::process;
 
-struct Config {
-    query: String,
-    filename: String,
-}
+use minigrep::Config;
 
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -35,20 +19,8 @@ fn main() {
     println!("In file {}", config.filename);
 
     // Ok has no value => handle err
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
     }
-}
-
-// Box<Error> means the function will return a type that implements the Error trait
-fn run(config: Config) -> Result<(), Box<Error>> {
-    let mut f = File::open(config.filename)?;
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
 }
