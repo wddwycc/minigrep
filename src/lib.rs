8b6c75dea6d558e70d47efb5715754a945b7ec78
闻端Carl <wddwyss@gmail.com>
2017-10-23 23:59:47 +0900

    search testcase

--- src/lib.rs
+++ src/lib.rs
@@ -32,3 +32,35 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 
     Ok(())
 }
+
+// lifetime parameters specify which argument lifetime is connected to the lifetime of the return value
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+}
